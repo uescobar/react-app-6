19bc1b29f254cb6aab7eae3ba2d614b0778f7b03
uescobar <uescobar.360@gmail.com>
2026-04-24 17:13:59 -0600

    Detalles del juego

--- src/pages/Home/Games/GameCard.tsx
+++ src/pages/Home/Games/GameCard.tsx
@@ -16,7 +16,12 @@ type Props = {
 
 export default function GameCard({ game }: Props) {
   return (
-    <Center py={6}>
+    <div
+      onClick={() => {
+        window.location.href = `https://www.cheapshark.com/redirect?dealID=${game.dealID}`;
+      }}
+      style={{ cursor: "ponter" }}
+    >
       <Box
         maxW={"445px"}
         w={"full"}
@@ -34,12 +39,7 @@ export default function GameCard({ game }: Props) {
           mb={6}
           pos={"relative"}
         >
-          <Image
-            h={"210px"}
-            src={
-              "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-            }
-          />
+          <Image h={"210px"} src={game.thumb} />
         </Box>
         <Stack>
           <Text
@@ -49,32 +49,26 @@ export default function GameCard({ game }: Props) {
             fontSize={"sm"}
             letterSpacing={1.1}
           >
-            Blog
+            {game.isOnSale == "1" ? "Oferta" : "Venta normal"}
           </Text>
           <Heading
             color={useColorModeValue("gray.700", "white")}
             fontSize={"2xl"}
             fontFamily={"body"}
           >
-            Boost your conversion rate
+            {game.title}
           </Heading>
-          <Text color={"gray.500"}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
+          <Text color={"gray.500"}>Crítica: {game.steamRatingText}</Text>
         </Stack>
         <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-          <Avatar
-            src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
-          />
           <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
+            <Text fontWeight={600}>Puntaje: {game.metacriticScore}</Text>
+            <Text color={"gray.500"}>
+              Antes: {game.normalPrice} - Ahora: {game.salePrice}
+            </Text>
           </Stack>
         </Stack>
       </Box>
-    </Center>
+    </div>
   );
 }
